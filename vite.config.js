857d1bbb11b463146e29f093295d3bf7c9f893fe
alemminik <egorsvincov@vk.com>
2026-02-11 20:05:09 +0300

    feat: init

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import injectHTML from 'vite-plugin-html-inject';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+
+export default defineConfig({
+  root: './src',
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: './src/index.html',
+      },
+    },
+  },
+  plugins: [
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+    }),
+    injectHTML(),
+    ViteMinifyPlugin(),
+  ],
+});
