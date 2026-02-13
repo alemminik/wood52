d73d5f96ffcea5075211eb57cf9b4d1496b22d77
alemminik <egorsvincov@vk.com>
2026-02-13 21:58:49 +0300

    fix: hero

--- src/js/components/home/hero-slider.js
+++ src/js/components/home/hero-slider.js
@@ -5,10 +5,12 @@ export const useHeroSlider = () => {
   new Swiper('.hero__slider', {
     slidesPerView: 'auto',
     spaceBetween: 15,
+    centeredSlides: true,
     loop: true,
     breakpoints: {
       476: {
         spaceBetween: 25,
+        centeredSlides: false,
       },
       721: {
         spaceBetween: 35,
